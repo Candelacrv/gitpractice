7e25052368f115a27a52fa08143d28e15410c8bc
unknown <crv2604.k@gmail.com>
2022-05-30 19:47:39 +0200

    no tira

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 print ("Hola desde git")
 print ("Hola desde la rama develop")
 <<<<<<< HEAD
-print ("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")
+print ("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")
 =======
 print ("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")
 >>>>>>> develop

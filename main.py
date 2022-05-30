f3e9eaa3e672dd0fa5d868efb45bb715c195e184
unknown <crv2604.k@gmail.com>
2022-05-30 19:55:34 +0200

    otro intento

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 print ("Hola desde git")
 print ("Hola desde la rama develop")
-<<<<<<< HEAD
-print ("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")
-=======
-print ("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")
->>>>>>> develop
+print ("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")
+
